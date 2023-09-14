7e55dacfc79e02a59d890ca0cad1ced4acd878f0
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-15 00:55:31 +0600

    code commit

---
+++ Context/constants.js
@@ -0,0 +1,12 @@
+// TheBlockchainCoders: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// TokenSale: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+
+
+import TheBlockchainCoders from './TheBlockchainCoders.json';
+import TokenSale from './TokenSale.json';
+
+export const TOKEN_ADDRESS = " 0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const TOKEN_ABI = TheBlockchainCoders.abi;
+
+export const TOKEN_SALE_ADDRESS = "0x199ad5fDaa08B7be4407e4e56F3a76773468E24d";
+export const TOKEN_SALE_ABI = TokenSale.abi;
