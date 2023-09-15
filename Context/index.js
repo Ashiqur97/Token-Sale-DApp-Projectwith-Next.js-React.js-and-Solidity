ff3df4d188c49441307b1ceb1ab2b8d0d8c1ed30
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-15 18:09:02 +0600

    code commit change

--- Context/index.js
+++ Context/index.js
@@ -13,6 +13,36 @@ const StateContext = createContext();
 
 export const StateContextProvider = ({children}) => {
     const TOKEN_ICO = "TOKEN SALL DAPP";
+
+    const [address,setAddress] = useState("");
+    const [balance,setBalance] = useState("");
+    const [nativeToken,setNativeToken] = useState("");
+    const [tokenHolders,setTokenHolders] = useState([]);
+    const [tokenSale,setTokenSale] = useState("");
+    const [currentHolder,setCurrentHolder] = useState("");
+
+    const fetchInitialData = async () => {
+        try {
+            const account = await CheckIfWalletConnected();
+
+            const balance = await getBalance();
+            setBalance(ethers.utils.formatEther(balance.toString()));
+            setAddress(account);
+
+            const TOKEN_CONTRACT = await connectingTOKENCONTRACT();
+
+            let tokenbalance;
+            if(account){
+                tokenbalance = await TOKEN_CONTRACT.balanceOf(account);
+            } else {
+                tokenbalance = 0;
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <StateContext.Provider value={{TOKEN_ICO}}>
             {children}
