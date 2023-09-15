d3a3fed1c0a9411e627bd64249aaa186fbec12fb
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-15 17:18:51 +0600

    code change

--- Context/index.js
+++ Context/index.js
@@ -1,20 +1,23 @@
 import React,{useState,useEffect,createContext,useContext} from 'react'
-import {ethers} from "ethers";
+import { ethers } from 'ethers'
 
 import {
     CheckIfWalletConnected,
-    connectWallet,
+    connectedWallet,
     connectingTOKENCONTRACT,
     getBalance,
-    connectingTOKENCONTRACT 
-} from "../Utils/index.js";
+    connectingTOKEN_SALE_CONTRACT
+} from "../Utils/index";
 
 const StateContext = createContext();
 
 export const StateContextProvider = ({children}) => {
     const TOKEN_ICO = "TOKEN SALL DAPP";
-
-    return <StateContextProvider value={{TOKEN_ICO}}>
-        {children}
-    </StateContextProvider>
+    return (
+        <StateContext.Provider value={{TOKEN_ICO}}>
+            {children}
+        </StateContext.Provider>
+    )
 }
+
+export const useStateContext = () => useContext(StateContext);
