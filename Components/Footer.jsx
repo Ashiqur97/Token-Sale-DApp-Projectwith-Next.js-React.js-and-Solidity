347b7d8cb47a2e7306b1cfdec62a27ac8173d619
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-16 16:22:53 +0600

    code commit change

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const footerList = ["Cryptocash","How its work","Token","FAQ","Contact"];
+  return <footer>
+    <div 
+    className="top_footer bg_light_dark"
+    data-z-index="1"
+    data-parallax="scroll"
+    data-image-src="assets/images/footer_bg.png"
+    >
+      <div className="container">
+          <div className="row">
+              <div className="col-lg-4 col-md-12">
+                  <div 
+                  className="footer_logo mb-3 animation"
+                  data-animation="fadeInUp"
+                  data-animation-deplay="0.2s"
+                  >
+                      <a href="#home_section" className="page-scroll">
+                          <img src="assets/images/footer_logo.png" alt="" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </footer>;
 };
 
 export default Footer;
