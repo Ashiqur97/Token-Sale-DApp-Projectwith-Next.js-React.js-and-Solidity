7202fb2d514af19c7c0a8b73afc6787fc5a6bd9a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-18 13:24:33 +0600

    code fixed

--- Components/About.jsx
+++ Components/About.jsx
@@ -4,7 +4,7 @@ import {BsArrowRight} from 'react-icons/bs'
 const About = () => {
   return <section id="about" className="small_pt">
     <div className="container">
-      <div className="row slign-items-center">
+      <div className="row align-items-center">
           <div className="col-lg-6 col-md-12 col-sm-12">
               <div className="text_md_center">
                   <img src="assets/images/about_img2.png" 
@@ -23,7 +23,38 @@ const About = () => {
                   data-animation="fadeInUp">
                       About The Cryptocash
                   </h4>
+                  <p className="animation" 
+                  data-animation-delay="0.2s" 
+                  data-animation="fadeInUp">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                      Eum cupiditate at id, animi deleniti quasi accusamus accusantium exercitationem 
+                      in voluptate veritatis aut molestias alias commodi laudantium adipisci a omnis perspiciatis.
+                  </p>
+
+                  <p className="animation" 
+                  data-animation-delay="0.4s" 
+                  data-animation="fadeInUp">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                      Eum cupiditate at id, animi deleniti quasi accusamus accusantium exercitationem 
+                      in voluptate veritatis aut molestias alias commodi laudantium adipisci a omnis perspiciatis.
+                  </p>
+
+                  <p className="animation" 
+                  data-animation-delay="0.8s" 
+                  data-animation="fadeInUp">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                      Eum cupiditate at id, animi deleniti quasi accusamus accusantium exercitationem 
+                      in voluptate veritatis aut molestias alias commodi laudantium adipisci a omnis perspiciatis.
+                  </p>
               </div>
+
+              <a  href=""
+              className="btn btn-default btn-radius video animation"
+              data-animation-delay="1s"
+              data-animation="fadeInUp"
+              >
+                let's Start <BsArrowRight />
+              </a>
           </div>
       </div>
     </div>
