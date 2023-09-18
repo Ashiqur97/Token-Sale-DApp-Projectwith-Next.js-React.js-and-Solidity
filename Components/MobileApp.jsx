b6692d4ddf664987bb0022dfb665c2bd7d2b6a3e
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-09-19 01:13:48 +0600

    code fixed

--- Components/MobileApp.jsx
+++ Components/MobileApp.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import {IoLogoAndroid} from "react-icons/io";
+import {AiFillAndroid} from "react-icons/ai";
 
 const MobileApp = () => {
-  return <div>MobileApp</div>;
+  return <section 
+  id="mobileapp" 
+  className="bg_light_dark" 
+  data-z-index="1" 
+  data-parallax="scroll"
+  data-image-src="assets/images/app_bg.png"
+  >
+    <div className="container">
+        <div className="row align-items-center">
+            <div className="col-lg-7 col-md-12 col-sm-12">
+                <div className="title_default_light title_border text_md_center">
+                    <h4 className="animation"
+                      data-animation="fadeInUp"
+                      data-animation-delay="0.2s"
+                    >
+                      Download Mobile App
+                    </h4>
+                    <p className="animation"
+                      data-animation="fadeInUp"
+                      ddata-animation-delay="0.2s"
+                    >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum omnis, 
+                      eaque modi quasi autem eius molestiae iusto perspiciatis eos voluptatum minima quia, 
+                      odit exercitationem quibusdam. Quod expedita cupiditate impedit natus!
+                    </p>
+
+                    <p className="animation"
+                      data-animation="fadeInUp"
+                      ddata-animation-delay="0.2s"
+                    >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum omnis, 
+                      eaque modi quasi autem eius molestiae iusto perspiciatis eos voluptatum minima quia, 
+                      odit exercitationem quibusdam. Quod expedita cupiditate impedit natus!
+                    </p>
+                </div>
+                <div className="btn_group text_md_center animation"
+                  data-animation="fadeInUp"
+                  data-animation-delay="0.6s"
+                >
+                  <a className="btn btn-default btn-radius">
+                    <AiFillAndroid className="new_font_size" />
+                    Google Store
+                  </a>
+                </div>
+            </div>
+        </div>
+      </div>
+  </section>
 };
 
 export default MobileApp;
